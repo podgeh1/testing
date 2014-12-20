85108d5fff762e4c8817bf192ceaea1ae23596c1
Podge Hession <C13329991@mydit.ie>
2014-12-20 10:05:11 +0000

    created uiButton

--- GitUseExample/AppDelegate.swift
+++ GitUseExample/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // Override point for customization after application launch.
+        println("application did not finish loading")
         return true
     }
 
